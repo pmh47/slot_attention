84561953534379d6a59c295398317ad78a14d689
Titas Anciukevičius <titas.anciukevicius@gmail.com>
2022-05-12 14:53:46 +0100

    Edited evaluation script to include out_path and num_scenes.

--- object_discovery/evaluate_all.sh
+++ object_discovery/evaluate_all.sh
@@ -11,12 +11,13 @@ ARROW_RESULTS_FILENAME=./arrow_slot-attn_results.txt
 ARROW_NUM_SLOTS=5
 
 
-GQN_OOD_SPLITS="OOD1 OOD6 OOD7 OOD8 OOD9 OODfgbg OODposition OODviews OODviews2"
+GQN_OOD_SPLITS="OOD1 OOD5 OOD6 OOD7 OOD8 OOD9 OODfgbg OODposition OODviews OODviews2 test"
 GQN_RESOLUTION=80
 
-ARROW_OOD_SPLITS="OODviews OODposition OODseven OODsix OODfive OODone"
+ARROW_OOD_SPLITS="OODviews OODposition OODseven OODsix OODfive OODone OODcomposition test"
 ARROW_RESOLUTION=96
 
+NUM_SCENES=100
 
 set -e
 
@@ -32,11 +33,13 @@ fi
 for SPLIT in $GQN_OOD_SPLITS ; do
 PYTHONPATH=.:src python slot_attention/object_discovery/evaluate.py \
     --data_path $GQN_DATA_PATH \
-    --split $SPLIT \
+    --split "$SPLIT" \
     --ckpt_path $GQN_CKPT_PATH \
     --num_slots $GQN_NUM_SLOTS \
+    --num_scenes "$NUM_SCENES" \
     --resolution $GQN_RESOLUTION \
-    --metrics_filename /tmp/results.txt
+    --metrics_filename /tmp/results.txt \
+    --out_path slot_attention/eval_output/gqn/"$SPLIT"
 echo "$SPLIT:" >> $GQN_RESULTS_FILENAME
 cat /tmp/results.txt >> $GQN_RESULTS_FILENAME
 done
@@ -45,11 +48,13 @@ done
 for SPLIT in $ARROW_OOD_SPLITS ; do
 PYTHONPATH=.:src python slot_attention/object_discovery/evaluate.py \
     --data_path $ARROW_DATA_PATH \
-    --split $SPLIT \
+    --split "$SPLIT" \
     --ckpt_path $ARROW_CKPT_PATH \
     --num_slots $ARROW_NUM_SLOTS \
+    --num_scenes "$NUM_SCENES" \
     --resolution $ARROW_RESOLUTION \
-    --metrics_filename /tmp/results.txt
+    --metrics_filename /tmp/results.txt \
+    --out_path slot_attention/eval_output/arrow/"$SPLIT"
 echo "$SPLIT:" >> $ARROW_RESULTS_FILENAME
 cat /tmp/results.txt >> $ARROW_RESULTS_FILENAME
 done
